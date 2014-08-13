a3df90240b94df26efddc6ab5dee2495cf9d96a5
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-13 23:59:35 +0530

    feat(init): yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name messMenuApp
+ * @description
+ * # messMenuApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('messMenuApp', [
+    'ngSanitize',
+    'ngTouch'
+  ]);
